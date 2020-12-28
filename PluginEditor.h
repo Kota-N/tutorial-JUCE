6bd9cf4e4a250d1b2f665bcaa3f4438841b4c2bf
Kota <kotancell@gmail.com>
2020-12-27 22:50:01 -0500

    Initial commit

---
+++ PluginEditor.h
@@ -0,0 +1,36 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class TutorialAudioProcessorEditor  : public juce::AudioProcessorEditor, private juce::Slider::Listener
+{
+public:
+    TutorialAudioProcessorEditor (TutorialAudioProcessor&);
+    ~TutorialAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    void sliderValueChanged(juce::Slider* slider) override;
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    TutorialAudioProcessor& audioProcessor;
+    
+    juce::Slider midiVolume;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (TutorialAudioProcessorEditor)
+};
